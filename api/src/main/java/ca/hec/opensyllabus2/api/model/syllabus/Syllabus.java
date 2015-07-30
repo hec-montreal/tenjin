58ffab0bdbbe36cf43db3f25679618d7c55df463
curtis.van-osch@hec.ca <curtis.van-osch@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-07-30 15:32:34 +0000

    ZCII-1851 : fix les POJOs de syllabus pour utiliser les nouveaux objets de template
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10281 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- api/src/main/java/ca/hec/opensyllabus2/api/model/syllabus/Syllabus.java
+++ api/src/main/java/ca/hec/opensyllabus2/api/model/syllabus/Syllabus.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package ca.hec.opensyllabus2.api.model.syllabus;
 import java.util.Date;
@@ -7,20 +7,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import ca.hec.opensyllabus2.api.model.template.Template;
+
 import lombok.Data;
 
 /**
  * <p>
  * CatalogDescription is the object for catalog descriptions for use with the Catalog Description Manager tool.
- * 
+ *
  * Does not implement org.sakaiproject.entity.api.Entity, though it could (but be careful with getId() as it exists there too but returns int)
  * </p>
  */
 
 
 @Data
-public class Syllabus {	
-	
+public class Syllabus {
+
 	protected final static String COURSE_ID = "id";
 	protected final static String SECTIONS = "sections";
 	protected final static String COURSE_TITLE = "coursetitle";
@@ -73,57 +75,57 @@ public class Syllabus {
 	protected final static String RESOURCE_VOLUME = "resource.volume";
 	protected final static String RESOURCE_YEAR = "resource.year";
 	protected final static String RESOURCE_LIBRARY = "resource.library";
-	
-	
+
+
     private Long syllabus_id;
-    
+
     private String course_id;
-    
+
     private String courseTitle;
-   
-    private Long template_id;
-    
+
+    private Template template;
+
     private String locale;
-    
+
     private String createdBy;
-    
+
     private Date creationDate;
-    
+
     private Set<SyllabusStructure> syllabusStructures;
-    
+
     public Syllabus (String course_id, String courseTitle, Long template_id){
     	this.course_id = course_id;
     	this.courseTitle = courseTitle;
-    	this.template_id = template_id;
+    	this.template = template;
     }
-    
+
     public Syllabus (){
-    	
+
     }
-    
+
     public void setSyllabusStructures( Set<SyllabusStructure> syllabusStructures){
     	this.syllabusStructures = syllabusStructures;
     }
-    
+
     public boolean equals(Object other) {
         if (this == other) return true;
         if ( !(other instanceof Syllabus) ) return false;
-        
+
         final Syllabus syllabus = (Syllabus) other;
-        
+
         if (this.syllabusStructures == syllabus.getSyllabusStructures())
         	return true;
-        
+
         return false;
     }
-    
+
     public int hashCode() {
         int result;
         result = this.syllabusStructures.hashCode();
         result = 29 * result ;
         return result;
     }
-    
+
     public String toString(){
     	return getCourseTitle();
     }
@@ -131,13 +133,13 @@ public class Syllabus {
     public Map <String, Object> getModel(){
 
 		Map <String, Object> model = new HashMap <String, Object>();
-		
+
 		//Retrieve syllabus properties
 		model.put(COURSE_ID, getCourse_id());
 		model.put(COURSE_TITLE, getCourseTitle());
-		model.put(TEMPLATE_ID, getTemplate_id());
+		model.put(TEMPLATE_ID, getTemplate());
 		model.put(LOCALE, getLocale());
-		
+
 		return model;
     }
 }
