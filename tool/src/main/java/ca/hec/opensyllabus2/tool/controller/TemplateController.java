667a1bc6b209489a3596f9ec041a1119cf961d44
curtis.van-osch@hec.ca <curtis.van-osch@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-08-03 17:36:29 +0000

    ZCII-1855 : renvoyer le Template du DAO
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10287 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- tool/src/main/java/ca/hec/opensyllabus2/tool/controller/TemplateController.java
+++ tool/src/main/java/ca/hec/opensyllabus2/tool/controller/TemplateController.java
@@ -3,16 +3,20 @@ package ca.hec.opensyllabus2.tool.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import ca.hec.opensyllabus2.api.Syllabus2Service;
 import ca.hec.opensyllabus2.api.model.template.Template;
-
 
 @Controller
 @RequestMapping(value ="v1/template")
@@ -20,11 +24,15 @@ public class TemplateController {
 
 	private static Log log = LogFactory.getLog(TemplateController.class);
 
+	@Setter
+	@Getter
+	@Autowired
+	private Syllabus2Service osyl2Service = null;
+
 	@RequestMapping(value = "/{templateId}", method = RequestMethod.GET)
-	public ModelAndView getTemplate(@PathVariable String templateId) {
+	public ModelAndView getTemplate(@PathVariable Long templateId) {
 
-		Template t = new Template(1L, "template title 1", true);
-
+		Template t = osyl2Service.getTemplate(templateId);
 
 		Map test = new HashMap<String, String>();
 		test.put("id", t.getTemplate_id());
