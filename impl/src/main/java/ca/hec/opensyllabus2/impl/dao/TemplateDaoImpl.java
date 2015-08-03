9b31285162543844e98a3006e04740374619aaca
curtis.van-osch@hec.ca <curtis.van-osch@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-08-03 17:34:34 +0000

    ZCII-1853 : Creation du DAO
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10286 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

---
+++ impl/src/main/java/ca/hec/opensyllabus2/impl/dao/TemplateDaoImpl.java
@@ -0,0 +1,29 @@
+package ca.hec.opensyllabus2.impl.dao;
+
+import java.util.List;
+import java.util.Set;
+
+import lombok.Setter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.sakaiproject.db.api.SqlService;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import ca.hec.opensyllabus2.api.dao.*;
+import ca.hec.opensyllabus2.api.model.syllabus.Syllabus;
+import ca.hec.opensyllabus2.api.model.syllabus.SyllabusStructure;
+import ca.hec.opensyllabus2.api.model.template.Template;
+
+public class TemplateDaoImpl extends HibernateDaoSupport implements TemplateDao {
+
+	private Log log = LogFactory.getLog(TemplateDaoImpl.class);
+
+	@Override
+	public Template getTemplate(Long templateId) {
+		return getHibernateTemplate().get(Template.class, templateId);
+	}
+}
