bed0ca3f2c4eafb2a3e33a12fb4802158a1b16d7
curtis.van-osch@hec.ca <curtis.van-osch@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-08-03 17:31:40 +0000

    ZCII-1854 : ajout de la methode getTemplate au Syllabus2Service
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10285 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- api/src/main/java/ca/hec/opensyllabus2/api/Syllabus2Service.java
+++ api/src/main/java/ca/hec/opensyllabus2/api/Syllabus2Service.java
@@ -3,10 +3,11 @@ package ca.hec.opensyllabus2.api;
 import java.util.List;
 
 import ca.hec.opensyllabus2.api.model.syllabus.Syllabus;
+import ca.hec.opensyllabus2.api.model.template.Template;
 
 /**
  * An interface to abstract all Sakai related API calls in a central method that can be injected into our app.
- * 
+ *
  * @author Mike Jennings (mike_jennings@unc.edu)
  *
  */
@@ -17,48 +18,48 @@ public interface Syllabus2Service {
 	 * @return
 	 */
 	public String getCurrentSiteId();
-	
+
 	/**
 	 * Get current user id
 	 * @return
 	 */
 	public String getCurrentUserId();
-	
+
 	/**
 	 * Get current user display name
 	 * @return
 	 */
 	public String getCurrentUserDisplayName();
-	
+
 	/**
 	 * Is the current user a superUser? (anyone in admin realm)
 	 * @return
 	 */
 	public boolean isSuperUser();
-	
+
 	/**
 	 * Post an event to Sakai
-	 * 
+	 *
 	 * @param event			name of event
 	 * @param reference		reference
 	 * @param modify		true if something changed, false if just access
-	 * 
+	 *
 	 */
 	public void postEvent(String event,String reference,boolean modify);
-	
+
 	/**
 	 * Wrapper for ServerConfigurationService.getString("skin.repo")
 	 * @return
 	 */
 	public String getSkinRepoProperty();
-	
+
 	/**
 	 * Gets the tool skin CSS first by checking the tool, otherwise by using the default property.
 	 * @param	the location of the skin repo
 	 * @return
 	 */
 	public String getToolSkinCSS(String skinRepo);
-	
+
 
 	/**
 	 * Retrieves the syllabus associated to a specific section and sectionId.
@@ -67,18 +68,25 @@ public interface Syllabus2Service {
 	 * @return
 	 */
 	public Syllabus getSyllabus (String courseId, String sectionId );
-	
+
 	/**
 	 * Retrieves the syllabus at the top of the hierarchy associated to the courseId.
 	 * @param courseId
 	 * @return
 	 */
 	public Syllabus getShareableSyllabus (String courseId);
-	
+
 	/**
 	 * Retrieves the syllabus content shared by the list of sectionIds.
 	 * @param sectionIds
 	 * @return
 	 */
 	public Syllabus getCommonSyllabus (String courseId, String[] sectionIds);
+
+	/**
+	 * Retrieves the template
+	 * @param templateId
+	 * @return
+	 */
+	public Template getTemplate(Long templateId);
 }
--- impl/src/main/java/ca/hec/opensyllabus2/impl/Syllabus2ServiceImpl.java
+++ impl/src/main/java/ca/hec/opensyllabus2/impl/Syllabus2ServiceImpl.java
@@ -17,31 +17,33 @@ import org.sakaiproject.user.api.UserNotDefinedException;
 
 import ca.hec.opensyllabus2.api.Syllabus2Service;
 import ca.hec.opensyllabus2.api.dao.Syllabus2Dao;
+import ca.hec.opensyllabus2.api.dao.TemplateDao;
 import ca.hec.opensyllabus2.api.model.syllabus.Syllabus;
+import ca.hec.opensyllabus2.api.model.template.Template;
 
 /**
  * Implementation of {@link Syllabus2Service}
- * 
- * 
+ *
+ *
  */
 public class Syllabus2ServiceImpl implements Syllabus2Service {
 
 	private static final Logger log = Logger.getLogger(Syllabus2ServiceImpl.class);
-    
+
 	/**
  	* {@inheritDoc}
  	*/
 	public String getCurrentSiteId(){
 		return toolManager.getCurrentPlacement().getContext();
 	}
-	
+
 	/**
  	* {@inheritDoc}
  	*/
 	public String getCurrentUserId() {
 		return sessionManager.getCurrentSessionUserId();
 	}
-	
+
 
 	/**
  	* {@inheritDoc}
@@ -49,79 +51,82 @@ public class Syllabus2ServiceImpl implements Syllabus2Service {
 	public String getCurrentUserDisplayName() {
 	   return userDirectoryService.getCurrentUser().getDisplayName();
 	}
-	
+
 	/**
  	* {@inheritDoc}
  	*/
 	public boolean isSuperUser() {
 		return securityService.isSuperUser();
 	}
-	
+
 	/**
  	* {@inheritDoc}
  	*/
 	public void postEvent(String event,String reference,boolean modify) {
 		eventTrackingService.post(eventTrackingService.newEvent(event,reference,modify));
 	}
-	
+
 	/**
  	* {@inheritDoc}
  	*/
 	public String getSkinRepoProperty(){
 		return serverConfigurationService.getString("skin.repo");
 	}
-	
+
 	/**
  	* {@inheritDoc}
  	*/
 	public String getToolSkinCSS(String skinRepo){
-		
-		String skin = siteService.findTool(sessionManager.getCurrentToolSession().getPlacementId()).getSkin();			
-		
+
+		String skin = siteService.findTool(sessionManager.getCurrentToolSession().getPlacementId()).getSkin();
+
 		if(skin == null) {
 			skin = serverConfigurationService.getString("skin.default");
 		}
-		
+
 		return skinRepo + "/" + skin + "/tool.css";
 	}
-	
+
 	/**
 	 * init - perform any actions required here for when this bean starts up
 	 */
 	public void init() {
 		log.info("init");
 	}
-	
+
     @Setter
 	private Syllabus2Dao syllabusDao;
-	
+
+    @Setter
+	private TemplateDao templateDao;
+
 	@Setter
 	private ToolManager toolManager;
-	
+
     @Setter
 	private SessionManager sessionManager;
-	
+
     @Setter
 	private UserDirectoryService userDirectoryService;
-	
+
     @Setter
 	private SecurityService securityService;
-	
+
     @Setter
 	private EventTrackingService eventTrackingService;
-	
+
     @Setter
 	private ServerConfigurationService serverConfigurationService;
-	
+
     @Setter
 	private SiteService siteService;
 
-	
+
     public Syllabus getShareableSyllabus(String courseId) {
-		// TODO check if the user is allowed to get the syllabus before 
-		
+		// TODO check if the user is allowed to get the syllabus before
+
 		Syllabus syllabus;
-		
+
 		try {
 			syllabus = syllabusDao.getShareableSyllabus(courseId);
 			return syllabus;
@@ -129,14 +134,14 @@ public class Syllabus2ServiceImpl implements Syllabus2Service {
 			log.warn("The syllabus could not be retrieved because: " + e.getMessage()) ;
 			return null;
 		}
-		
-		
+
+
 	}
 
 	@Override
 	public Syllabus getSyllabus(String courseId, String sectionId) {
 Syllabus syllabus;
-		
+
 		try {
 			syllabus = syllabusDao.getSyllabus(courseId, sectionId);
 			return syllabus;
@@ -144,14 +149,14 @@ Syllabus syllabus;
 			log.warn("The syllabus could not be retrieved because: " + e.getMessage()) ;
 			return null;
 		}
-		
+
 	}
 
-	
+
 	@Override
 	public Syllabus getCommonSyllabus(String courseId, String[]  sectionIds) {
 Syllabus syllabus;
-		
+
 		try {
 			syllabus = syllabusDao.getCommonSyllabus(courseId, sectionIds);
 			return syllabus;
@@ -159,6 +164,11 @@ Syllabus syllabus;
 			log.warn("The syllabus could not be retrieved because: " + e.getMessage()) ;
 			return null;
 		}
-		
+
+	}
+
+	@Override
+	public Template getTemplate(Long templateId) {
+		return templateDao.getTemplate(templateId);
 	}
 }
