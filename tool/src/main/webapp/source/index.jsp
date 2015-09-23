7e1c90fda2a1595a3c0eaa72488987d923126bd9
johan.julien@hec.ca <johan.julien@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-09-23 15:55:13 +0000

    ZCII - 1919 : Ajout du clic sur un élément de l'arbre + Mise a jour du contenu de la partie de droite + ajout fichier global.scss
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10371 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- tool/src/main/webapp/source/index.jsp
+++ tool/src/main/webapp/source/index.jsp
@@ -45,10 +45,10 @@
 	<div class="row">
 
 		<!-- Menu de gauche -->
-		<div data-ng-include="'/opensyllabus2-tool/dest/views/leftMenu.html'"   class="col-md-3 pull-left left-menu" id="menu" ></div>
+		<div data-ng-include="'/opensyllabus2-tool/dest/views/leftMenu.html'"   class="col-md-3 hide-tablet pull-left left-menu" id="menu" ></div>
 		
 		<!-- Container de droite -->
-		<div data-ng-include="'/opensyllabus2-tool/dest/views/contentPanel.html'"  class="col-md-9" id="content"></div>
+		<div data-ng-include="'/opensyllabus2-tool/dest/views/contentPanel.html'"  class="col-md-9 col-sm-12" id="content"></div>
 
 	</div>
 
--- tool/src/main/webapp/source/js/controllers/leftMenu/leftMenuCtrl.js
+++ tool/src/main/webapp/source/js/controllers/leftMenu/leftMenuCtrl.js
@@ -2,36 +2,7 @@
 opensyllabusApp.controller('LeftMenuCtrl', [ '$scope', function ($scope){
 	'use strict';
 
-	var menuTree = [];
-	var contentAttributes = {};
-	var contentTree = [];
-	var iterMenu = 0;
-	
-	var data = {};
-	data.syllabusStructures = [];
-
-	for ( var iterStruct = 0; iterStruct < data.syllabusStructures.length; iterStruct++ ){
-		if (data.syllabusStructures[iterStruct].displayPage ){
-			var menuItem = data.syllabusStructures[iterStruct];
-			if (menuItem.parent === null) {
-				menuItem.parent = "#";
-			}
-			menuItem.a_attr = "{\"ngclick\": \"loadElementAttributes("+ iterStruct+")\"}";
-			
-			contentAttributes.id = menuItem.id;
-			contentAttributes.elementAttributes = menuItem.elementAttributes;
-			console.log(menuItem.elementAttributes);
-			contentTree[iterMenu] = contentAttributes;
-			
-			menuTree[iterMenu] =menuItem;
-			iterMenu = iterMenu+1;
-			contentAttributes = {};
-			
-		}
-		
-	}		
-
-    $scope.montoggle = function (scope) {
+    $scope.toggleTree = function (scope) {
 		scope.toggle();
 	};
 	$scope.collapseAll = function () {
@@ -60,126 +31,44 @@ opensyllabusApp.controller('LeftMenuCtrl', [ '$scope', function ($scope){
 		});
 	};
 
-	$scope.loadContentPanel = function($item){
-		console.log($item.title);
-		// debugger;
+	$scope.select = function($item){
+		
+		if ($item.syllabusElement_id !== $scope.infos.currentItemId) {
+			console.time('select');
+			parcoursTree($scope.syllabus);
+			$item.selected = true;
+			$scope.infos.currentItemId = $item.syllabusElement_id;
+			console.timeEnd('select');
+		}
+
 	};
 
-	$scope.list = [{
-        'id': 1,
-        'title': 'Présentation',
-        'nodrag': true,
-        'items': []
-      }, 
-		{
-        'id': 2,
-        'title': 'Coordonnées',
-        'nodrag': true,
-        'items': []
-      },
-      {
-        'id': 3,
-        'title': 'Matériel pédagogique',
-        'nodrag': true,
-        'nodrop': true, // An arbitrary property to check in custom template for nodrop-enabled
-        'items': []
-      }, 
-      {
-        'id': 4,
-        'title': 'Evaluations',
-        'nodrag': true,
-        'items': [
-          {
-            'id': 41,
-            'title': 'Evaluation',
-            'items': []
-          },
-          {
-            'id': 42,
-            'title': 'Examen intra',
-            'items': []
-          }
-        ]
-      },
-      {
-        'id': 5,
-        'title': 'Organisation du cours',
-        'nodrag': true,
-        'items': [
-          {
-            'id': 51,
-            'title': 'Chapitre 1',
-            'items': [
-              {
-	            'id': 511,
-	            'title': 'Séance de cours',
-	            'items': []
-	          },
-	          {
-	            'id': 512,
-	            'title': 'Séance de travaux pratique',
-	            'items': []
-	          }   	
-            ]
-          },
-          {
-            'id': 52,
-            'title': 'Chapitre 2',
-            'items': [
-			{
-				'id': 521,
-				'title': 'Séance de cours 1',
-				'items': []
-			},
-			{
-				'id': 522,
-				'title': 'Séance de cours 2',
-				'items': []
-			}  
-            ]
-          }
-        ]
-      }
-      ];
+    /**
+     * Parcours de l'arbre
+     * @param {Object} $rootTree racine de l'arbre
+     */
+	var parcoursTree = function($rootTree) {
+		if ($rootTree.syllabusElements) {
+			for (var i = 0; i < $rootTree.syllabusElements.length; i++){
+				$rootTree.syllabusElements[i].selected = false;
+				parcoursTreeChildren($rootTree.syllabusElements[i]);
+			}
+		}
+	};
 
-	$scope.collapseAll();
+    /**
+     * Parcours de manière récursive les enfants de l'arbre
+     * @param {Object} $rootTree racine du sous-arbre
+     */
+	var parcoursTreeChildren = function($rootTree) {
+		if ($rootTree.children) {
+			for (var i = 0; i < $rootTree.children.length; i++){
+				$rootTree.children[i].selected = false;
+				parcoursTreeChildren($rootTree.children[i]);
+			}
+		}
+	};
 
-	// $('#menuTree').jstree({
-	// 	"core": {
-	//         "themes": {
-	//             "responsive": true
-	//         },
-	//         "check_callback": true,
-	//         'data': menuTree
-	//     },
-	//     "types": {
-	//         "default": {
-	//         	"icon" : "glyphicon glyphicon-flash"
-	//         },
-	        
-	//     },
-	//     "state": { "key": "syllabusTree" },
-	//     "plugins": ["contextmenu", "dnd", "state", "types", "wholerow"]
-	// });
-	
-	// $('#menuTree').on("changed.jstree", function (e, data) {
-	//     var select = data.selected[0]; 
-	//     document.getElementById("content").innerHTML = "";
-	//     console.log(select);
-	//     var contentAtt = {};
-	//     for ( iterMenu = 0;iterMenu < contentTree.length;iterMenu++){
-	//     	contentAtt = contentTree[iterMenu];
-	// 		if (contentAtt.id === select ){
-	// 			console.log(contentAtt.elementAttributes);
-	// 			 for ( var iterAtt = 0; iterAtt < contentAtt.elementAttributes.length;iterAtt++){
-	// 				 document.getElementById("content").innerHTML += "<p>" + contentAtt.elementAttributes[iterAtt].attributeKey + ": " + contentAtt.elementAttributes[iterAtt].attributeValue +"</p>";
-	// 			 }
-				
-	// 			break;
-	// 		}
-			
-	// 	}	
-	// });
 }]);
 
 
