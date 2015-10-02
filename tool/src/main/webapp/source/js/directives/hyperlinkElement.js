851e8d4c9d4f89e22d1670b2bbcc9834984a2b33
johan.julien@hec.ca <johan.julien@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-10-02 13:38:28 +0000

    ZCII-1937 : Ajout de la directive hyperlinkElement
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10396 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

---
+++ tool/src/main/webapp/source/js/directives/hyperlinkElement.js
@@ -0,0 +1,26 @@
+
+opensyllabusApp.directive('hyperlinkElement', function (){
+    'use strict';
+
+    return {
+        scope: {
+            element: '=hyperlinkElement'
+        },
+        restrict: 'A',
+        templateUrl: '/opensyllabus2-tool/views/hyperlinkElement.html',
+        controller: function ($scope) {
+
+        },
+        link: function ($scope, $element) {
+            if ($scope.element.attributes.comment){
+                debugger;
+                var elem = angular.element( $element[0].querySelector('.comment') );
+                // console.log("text element : " + $scope.element.attributes.text);
+                elem.html($scope.element.attributes.comment);
+            }
+        }
+
+    };
+
+});
+
