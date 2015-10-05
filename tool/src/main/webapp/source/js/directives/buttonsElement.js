97ad52a29855a772008c983926fb953e5a9ac5b0
johan.julien@hec.ca <johan.julien@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-10-05 15:23:55 +0000

    ZCII-1942 : Ajout de la directive image + ajout d'une directive de gestion des boutons édition et suppression
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10400 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

---
+++ tool/src/main/webapp/source/js/directives/buttonsElement.js
@@ -0,0 +1,21 @@
+
+opensyllabusApp.directive('buttonsElement', function (){
+    'use strict';
+
+    return {
+        scope: {
+            element: '=buttonsElement'
+        },
+        restrict: 'A',
+        templateUrl: '/opensyllabus2-tool/views/buttonsElement.html',
+        controller: function ($scope) {
+
+        },
+        link: function ($scope, $element) {
+
+        }
+
+    };
+
+});
+
--- tool/src/main/webapp/source/js/directives/imageElement.js
+++ tool/src/main/webapp/source/js/directives/imageElement.js
@@ -1,21 +1,21 @@
 
-opensyllabusApp.directive('documentElement', function (){
+opensyllabusApp.directive('imageElement', function (){
     'use strict';
 
     return {
         scope: {
-            element: '=documentElement'
+            element: '=imageElement'
         },
         restrict: 'A',
-        templateUrl: '/opensyllabus2-tool/views/documentElement.html',
+        templateUrl: '/opensyllabus2-tool/views/imageElement.html',
         controller: function ($scope) {
 
         },
         link: function ($scope, $element) {
-            if ($scope.element.attributes.docComment){
+            if ($scope.element.attributes.imageComment){
                 var elem = angular.element( $element[0].querySelector('.comment') );
                 // console.log("text element : " + $scope.element.attributes.text);
-                elem.html($scope.element.attributes.docComment);
+                elem.html($scope.element.attributes.imageComment);
             }
         }
 
