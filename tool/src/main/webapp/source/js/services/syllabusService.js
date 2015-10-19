f0c03228eec054a6ecac248ce3a6a07dca5de4ce
mame-awa.diop@hec.ca <mame-awa.diop@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-10-19 15:32:16 +0000

    Commentaire de la référence à localhost
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10438 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- tool/src/main/webapp/source/js/services/syllabusService.js
+++ tool/src/main/webapp/source/js/services/syllabusService.js
@@ -2,7 +2,7 @@ opensyllabusApp.service('SyllabusService', ['$resource', function ($resource){
     'use strict';
 
     //TODO: la verification du nom du param (et de la validité du param ?) se fera sur le cote client
-     var syllabusProvider = $resource('v1/syllabus/:courseId.json', 
+      var syllabusProvider = $resource('v1/syllabus/:courseId.json', 
    // var syllabusProvider = $resource('http://localhost:8080/portal/tool/409fef00-0acf-43f6-94dc-b346a3fb097f/v1/syllabus/:courseId.json', 
         {
             courseId : "@courseId"
