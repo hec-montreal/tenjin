ecda372ad1587fa2ebda5a99e80934f2a742bf54
mame-awa.diop@hec.ca <mame-awa.diop@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-11-11 19:49:26 +0000

    ZCII-1940: Développer le composante affichage "Évaluation" - Refactoring du code
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10479 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- tool/src/main/webapp/source/js/services/leftMenuServices.js
+++
@@ -1,8 +0,0 @@
-﻿var leftMenuServices = angular.module('leftMenuServices', ['ngResource']);
-
-leftMenuServices.factory('LeftMenu', ['$resource',
-function ($resource){
-	return $resource ('opensyllabus2-tool/:syllabus_structure.json', {}, {
-		query: {method:'GET', params:{}, isArray:true}
-	});
-}]);
