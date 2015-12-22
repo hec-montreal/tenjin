68917e856a6f04a360e5e436d17ca6e33281e53e
johan.julien@hec.ca <johan.julien@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2015-12-22 20:58:43 +0000

    [ZCII-1976] : Mise à jour sélecteur de document
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10576 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

--- tool/src/main/webapp/source/js/directives/form/browserElementForm.js
+++ tool/src/main/webapp/source/js/directives/form/browserElementForm.js
@@ -9,16 +9,22 @@ opensyllabusApp.directive('browserElementForm', ['ResourcesService', function (R
         templateUrl: 'form/browserElementForm.html',
         controller: function ($scope) {
             $scope.resources = ResourcesService.resources;
+
             $scope.browserOptions  = {
-                    name: "browserTree",
-                    accept: function(sourceNodeScope, destNodesScope, destIndex) {
-                                return false;
-                            },
+                name: "browserTree",
+                accept: function(sourceNodeScope, destNodesScope, destIndex) {
+                    return false;
+                },
             };
 
             $scope.toggleTree = function(scope){
                 scope.toggle();
-            }
+            };
+
+            $scope.setSelectedResource = function($item) {
+                $scope.selectedResource = $item;
+            };
+
             console.log($scope.resources);
 
         },
