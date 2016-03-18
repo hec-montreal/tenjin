3a459b78b93aadf40f6f1cfc20bec43e970185b3
curtis.van-osch@hec.ca <curtis.van-osch@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2016-03-18 15:16:23 +0000

    ZCII-2184 : ajout des permissions read et write pour tenjin
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10703 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

---
+++ api/src/main/java/ca/hec/opensyllabus2/api/TenjinFunctions.java
@@ -0,0 +1,9 @@
+package ca.hec.opensyllabus2.api;
+
+public interface TenjinFunctions {
+
+	public static final String TENJIN_FUNCTION_PREFIX = "tenjin.";
+	
+	public static final String TENJIN_FUNCTION_READ = TENJIN_FUNCTION_PREFIX + "read";
+	public static final String TENJIN_FUNCTION_WRITE = TENJIN_FUNCTION_PREFIX + "write";
+}
