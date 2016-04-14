ee1bc2080b2d48ced61cf31aa525d335501291ec
johan.julien@hec.ca <johan.julien@hec.ca@3bde9bb2-8d47-0410-8f2c-8033b91dd29c>
2016-04-14 13:33:23 +0000

    [FIX] : Correction fichier manquant hyperlinkelement
    git-svn-id: svn://havok.hec.ca/sakai/opensyllabus2/trunk@10766 3bde9bb2-8d47-0410-8f2c-8033b91dd29c

---
+++ tool/src/main/webapp/source/components/element/hyperlinkElement/hyperlinkElement.ts
@@ -0,0 +1,40 @@
+﻿import {Component, Input, Inject} from 'angular2/core';
+
+@Component({
+    selector: 'hyperlink-element',
+    template: `
+    <div class="element hyperlink"> 
+
+        <div class="titre-element">
+            <a data-nodrag [href]="element.attributes.hyperlinkUrl" target="_blank">
+                {{ (element.title ? element.title : element.attributes.hyperlinkUrl) }} 
+            </a>
+            <span class="link-type">
+                [{{ element.attributes.hyperlinkType }}] 
+            </span> 
+        </div>
+        <div class="url-lien">
+            ({{ element.attributes.hyperlinkUrl }}) 
+        </div>
+
+        <div [innerHTML]="element.description" class="comment"></div>
+
+        <div class="infos">
+            <span [ngClass]="{ 'picto-important' : ( element.attributes.hyperlinkImportant === 'true') }" title="Important"></span>
+            <span [ngClass]="{ 'picto-hec' : ( element.attributes.hyperlinkDiffusion === 'hec' ), 'picto-public' : ( element.attributes.hyperlinkDiffusion === 'public' ) }" title="Niveau de diffusion"></span> 
+            <span [ngClass]="{ 'picto-hidden' : ( element.attributes.hyperlinkHidden === 'true') }" title="Visible"></span>     
+        </div>
+
+    </div> 
+    `
+})
+
+
+export class HyperlinkElementComponent {
+
+    @Input() element: any;
+
+    constructor() {
+    }
+}
+
