42b578e2e25ecfe512b0338f345d365ab259ff2c
Mame Awa Diop <mame-awa.diop@hec.ca>
2016-10-21 14:53:43 -0400

    Rollback à Angular 1 de l'élément image

---
+++ tool/src/main/webapp/source/components/element/imageElement/imageElement.js
@@ -0,0 +1,19 @@
+tenjinApp.directive('imageElement', ['ResourcesService' ,function ( ResourcesService){
+    'use strict';
+
+    return {
+        scope: {
+            element: '=imageElement'
+        },
+        restrict: 'A',
+        templateUrl: 'element/imageElement/imageElement.html',
+        controller: function ($scope) {
+            $scope.resource = ResourcesService.getResource($scope.element.attributes.resourceId);
+        },
+        link: function ($scope, $element) {
+
+         }
+
+    };
+
+}]);
