963524480d56115ea547e3f332328e2f8056a7ad
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2016-10-24 17:17:16 -0400

    Fix bug affichage image-element

--- tool/src/main/webapp/source/components/element/imageElement/imageElement.js
+++ tool/src/main/webapp/source/components/element/imageElement/imageElement.js
@@ -1,19 +1,21 @@
-tenjinApp.directive('imageElement', ['ResourcesService' ,function ( ResourcesService){
+tenjinApp.directive('imageElement', ['ResourcesService', function(ResourcesService) {
     'use strict';
 
     return {
         scope: {
             element: '=imageElement'
         },
+
         restrict: 'A',
+
         templateUrl: 'element/imageElement/imageElement.html',
-        controller: function ($scope) {
+
+        controller: function($scope) {
             $scope.resource = ResourcesService.getResource($scope.element.attributes.resourceId);
         },
-        link: function ($scope, $element) {
 
-         }
+        link: function($scope, $element) {
 
+        }
     };
-
 }]);
