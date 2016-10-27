36dac80ebbb8dddb454f26430cd02783ded947b1
Curtis van Osch <curtis.van-osch@hec.ca>
2016-10-27 13:50:16 -0400

    ZCII-1992 : create the new examElement and its form

---
+++ tool/src/main/webapp/source/components/element/examElement/examElement.js
@@ -0,0 +1,23 @@
+﻿
+tenjinApp.directive('examElement', ['SyllabusService', 'TreeService', function (SyllabusService, TreeService){
+    'use strict';
+
+    return {
+        scope: {
+            element: '=examElement',
+            index: '='
+        },
+        restrict: 'A',
+        templateUrl: 'element/examElement/examElement.html',
+        controller: function ($scope) {
+            $scope.syllabusService = SyllabusService;
+            $scope.treeService = TreeService;
+        },
+        link: function ($scope, $element) {
+        	$scope.treeService = TreeService;
+        }
+
+    };
+
+}]);
+
---
+++ tool/src/main/webapp/source/components/form/examElementForm/examElementForm.js
@@ -0,0 +1,26 @@
+
+tenjinApp.directive('examElementForm',  function (){
+    'use strict';
+
+    return {
+        scope: {
+            element: '=examElementForm',
+            typelabel: '=' //Might need to link to rules for I18N purposes
+        },
+        restrict: 'A',
+        templateUrl: 'form/examElementForm/examElementForm.html',
+        controller: function ($scope) {
+
+        },
+        link: function ($scope, $element) {
+            
+          $scope.element.attributes.examType = $scope.typelabel;
+            
+        }
+
+    };
+
+});
+
+
+
