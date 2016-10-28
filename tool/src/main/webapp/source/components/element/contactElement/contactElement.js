63b4d995228dbea5a20042c2efd577c11fe6efc7
Curtis van Osch <curtis.van-osch@hec.ca>
2016-10-28 17:12:34 -0400

    Fix title on contact info element

--- tool/src/main/webapp/source/components/element/contactElement/contactElement.js
+++ tool/src/main/webapp/source/components/element/contactElement/contactElement.js
@@ -1,4 +1,4 @@
-﻿tenjinApp.directive('contactElement', [ function (){
+﻿tenjinApp.directive('contactElement', ['config', '$translate', function (config, $translate){
     'use strict';
 
     return {
@@ -9,6 +9,15 @@
         restrict: 'A',
         templateUrl: 'element/contactElement/contactElement.html',
         controller: function ($scope) {
+            // Retrieve the label for the title
+            if ($scope.element.attributes.contactInfoTitle && $scope.element.attributes.contactInfoTitle > 0) {
+                for (var i = 0; i < config.contactInfoTitles.length; i++) {
+                    if (parseInt($scope.element.attributes.contactInfoTitle) === config.contactInfoTitles[i].id) {
+                        $scope.title = $translate.instant(config.contactInfoTitles[i].name);
+                        break;
+                    }
+                }
+            }
         }        
     };
 
