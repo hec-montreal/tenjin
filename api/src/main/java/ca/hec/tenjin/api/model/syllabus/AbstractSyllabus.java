61594863569a840bb30c904dc69c9805200654fe
Curtis van Osch <curtis.van-osch@hec.ca>
2016-11-04 12:09:10 -0400

    ZCII-2557 : commit AbstractSyllabus

---
+++ api/src/main/java/ca/hec/tenjin/api/model/syllabus/AbstractSyllabus.java
@@ -0,0 +1,36 @@
+package ca.hec.tenjin.api.model.syllabus;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import ca.hec.tenjin.api.model.syllabus.provider.OfficialProvider;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+*
+* @author <a href="mailto:mame-awa.diop@hec.ca">Mame Awa Diop</a>
+* @author <a href="mailto:curtis.van-osch@hec.ca">Curtis van Osch</a>
+* @version $Id: $
+*/
+
+@Data
+public class AbstractSyllabus {
+	private Long id;
+    private String siteId;
+    private String courseTitle;
+    private String title;
+    private Long templateId;
+    private String locale;
+    private Boolean common;
+    private String createdBy;
+    private String createdByName;
+    private Date createdDate;
+    private String lastModifiedBy;
+    private Date lastModifiedDate;
+    private Boolean deleted;
+    private Boolean published;
+    private Set<String> sections;
+    private OfficialProvider provider;
+}
