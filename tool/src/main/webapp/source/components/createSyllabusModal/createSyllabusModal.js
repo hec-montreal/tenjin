ff8d03d3fff168900ea0a131bca7a971ae32f02a
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2016-12-13 16:49:03 -0500

    ZCII-2682

--- tool/src/main/webapp/source/components/createSyllabusModal/createSyllabusModal.js
+++ tool/src/main/webapp/source/components/createSyllabusModal/createSyllabusModal.js
@@ -1,86 +1,86 @@
 ﻿tenjinApp.controller('CreateSyllabusModalCtrl', ['$scope', '$uibModalInstance', '$translate', 'SyllabusService',  'UserService', function($scope, $uibModalInstance, $translate, SyllabusService, UserService) {
-    'use strict';
+	'use strict';
 
-    // Init syllabus data : name of syllabus + sections
-    $scope.data = {
-        'name': '',
-        'sections': []
-    };
+	// Init syllabus data : name of syllabus + sections
+	$scope.data = {
+		'name': '',
+		'sections': []
+	};
 
-    // get user sections with write permissions
-    for (var i = 0; i < UserService.profile.sections.length; i++) {
-        var sectionUser = UserService.profile.sections[i];
-        if (sectionUser.permissions.write === true) {
-            var section = angular.copy(sectionUser);
-            var syllabusList = SyllabusService.getSyllabusList();
+	// get user sections with write permissions
+	for (var i = 0; i < UserService.profile.sections.length; i++) {
+		var sectionUser = UserService.profile.sections[i];
+		if (sectionUser.permissions.write === true) {
+			var section = angular.copy(sectionUser);
+			var syllabusList = SyllabusService.getSyllabusList();
 
-            for (var j = 0; j < syllabusList.length; j++) {
-                if (syllabusList[j].sections.indexOf(section.id) > -1) {
-                    section.assignedSyllabus = syllabusList[j];
-                    break;
-                }
-            }
+			for (var j = 0; j < syllabusList.length; j++) {
+				if (syllabusList[j].sections.indexOf(section.id) > -1) {
+					section.assignedSyllabus = syllabusList[j];
+					break;
+				}
+			}
 
-            $scope.data.sections.push(section);
-        }
-    }
+			$scope.data.sections.push(section);
+		}
+	}
 
-    /**
-     * Callback ok button
-     */
-    $scope.ok = function() {
-        var common = SyllabusService.getCommonSyllabus();
+	/**
+	 * Callback ok button
+	 */
+	$scope.ok = function() {
+		var common = SyllabusService.getCommonSyllabus();
 
-        // keep only the selected sections
-        var sections = [];
+		// keep only the selected sections
+		var sections = [];
 
-        for (var i = 0; i < $scope.data.sections.length; i++) {
-            if ($scope.data.sections[i].checked === true) {
-                sections.push($scope.data.sections[i].id);
-            }
-        }
+		for (var i = 0; i < $scope.data.sections.length; i++) {
+			if ($scope.data.sections[i].checked === true) {
+				sections.push($scope.data.sections[i].id);
+			}
+		}
 
-        // init data for a new specific syllabus
-        // common is not loaded and templateId is null then it's not going to work
-        var newSyllabus = {
-            'id': null,
-            'courseTitle': UserService.profile.site.courseTitle,
-            'siteId': UserService.profile.site.courseId,
-            'sections': sections,
-            'title': $scope.data.name,
-            'common': false,
-            'templateId': (common ? common.templateId : null),
-            'elements': null,
-            'locale': 'fr_CA'
-        };
+		// init data for a new specific syllabus
+		// common is not loaded and templateId is null then it's not going to work
+		var newSyllabus = {
+			'id': null,
+			'courseTitle': UserService.profile.site.courseTitle,
+			'siteId': UserService.profile.site.courseId,
+			'sections': sections,
+			'title': $scope.data.name,
+			'common': false,
+			'templateId': (common ? common.templateId : null),
+			'elements': null,
+			'locale': 'fr_CA'
+		};
 
-        var savePromise = SyllabusService.save(newSyllabus);
-        SyllabusService.setWorking(true);
+		var savePromise = SyllabusService.save(newSyllabus);
+		SyllabusService.setWorking(true);
 
-        var syllabusAdded = null;
+		var syllabusAdded = null;
 
-        savePromise.$promise.then(function($data) {
-            // set write and publish permission on the new syllabus
-            SyllabusService.setWritePermission($data);
+		savePromise.$promise.then(function($data) {
+			// set write and publish permission on the new syllabus
+			SyllabusService.setWritePermission($data);
 
-            // refresh the list 
-            SyllabusService.syllabusList.push($data);
-            syllabusAdded = $data;
+			// refresh the list 
+			SyllabusService.syllabusList.push($data);
+			syllabusAdded = $data;
 
-        }, function($error) {
-            // syllabusAdded stay null
+		}, function($error) {
+			// syllabusAdded stay null
 
-        }).finally(function() {
-            SyllabusService.setWorking(false);
-            // close the popup
-            $uibModalInstance.close(syllabusAdded);
-        });
-    };
+		}).finally(function() {
+			SyllabusService.setWorking(false);
+			// close the popup
+			$uibModalInstance.close(syllabusAdded);
+		});
+	};
 
-    /**
-     * Callback cancel button
-     */
-    $scope.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
-    };
+	/**
+	 * Callback cancel button
+	 */
+	$scope.cancel = function() {
+		$uibModalInstance.dismiss('cancel');
+	};
 }]);
