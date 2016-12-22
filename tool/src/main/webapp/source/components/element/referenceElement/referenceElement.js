d906a815bea1215de2dc234f2ad04b1556f6812a
Mame Awa Diop <mame-awa.diop@hec.ca>
2016-12-22 11:44:24 -0500

    ZCII-2628: L'élément référence bibliographique n'est pas bien affiché

--- tool/src/main/webapp/source/components/element/referenceElement/referenceElement.js
+++ tool/src/main/webapp/source/components/element/referenceElement/referenceElement.js
@@ -1,4 +1,4 @@
-tenjinApp.directive('referenceElement', ['ResourcesService', function(ResourcesService) {
+tenjinApp.directive('referenceElement', ['config',  '$translate','ResourcesService', function(config, $translate, ResourcesService) {
 	'use strict';
 
 	return {
@@ -13,7 +13,23 @@ tenjinApp.directive('referenceElement', ['ResourcesService', function(ResourcesS
 		link: function($scope, $element) {
 			$scope.citation = ResourcesService.getResource($scope.element.attributes.citationId);
 
-			console.log($scope.citation);
+			var referenceTypeId = $scope.element.attributes.referenceType;
+
+            if (referenceTypeId) {
+                $scope.referenceTypeId = parseInt(referenceTypeId, 10);
+            } else {
+                $scope.referenceTypeId = -1;
+            }
+
+            // Retrieve the label for the reference type id
+            if ($scope.element.attributes.referenceType) {
+                for (var i = 0; i < config.referenceTypes.length; i++) {
+                    if (parseInt($scope.element.attributes.referenceType, 10) === config.referenceTypes[i].id) {
+                        $scope.referenceType = $translate.instant(config.referenceTypes[i].name);
+                        break;
+                    }
+                }
+            }
 
 			$scope.element.title = "Citation";
 
