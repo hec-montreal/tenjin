e71d89d53eeefdbb1e1f2f996fa74f176d386cf4
Curtis van Osch <curtis.van-osch@hec.ca>
2017-03-21 15:17:50 -0400

    ZCII-2801 : use hyperlink type name instead of index

--- tool/src/main/webapp/source/components/element/hyperlinkElement/hyperlinkElement.js
+++ tool/src/main/webapp/source/components/element/hyperlinkElement/hyperlinkElement.js
@@ -9,21 +9,8 @@ tenjinApp.directive('hyperlinkElement', ['config', '$translate', function(config
         templateUrl: 'element/hyperlinkElement/hyperlinkElement.html',
 
         controller: function($scope) {
-            var hyperlinkType = $scope.element.attributes.hyperlinkType;
-
-            if (hyperlinkType) {
-                $scope.hyperlinkTypeId = parseInt(hyperlinkType, 10);
-            } else {
-                $scope.hyperlinkTypeId = -1;
-            }
-
             if ($scope.element.attributes.hyperlinkType) {
-                for (var i = 0; i < config.hyperlinkTypes.length; i++) {
-                    if (parseInt($scope.element.attributes.hyperlinkType, 10) === config.hyperlinkTypes[i].id) {
-                        $scope.hyperlinkType = $translate.instant(config.hyperlinkTypes[i].name);
-                        break;
-                    }
-                }
+                $scope.hyperlinkType = $translate.instant($scope.element.attributes.hyperlinkType);
             }
         }
     };
