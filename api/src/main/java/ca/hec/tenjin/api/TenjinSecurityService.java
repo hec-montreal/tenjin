3a794cfefe905e551e73337bd0ba0d3655ef552b
Curtis van Osch <curtis.van-osch@hec.ca>
2017-03-24 16:48:22 -0400

    ZCII-2840 : correct security check on course outline

--- api/src/main/java/ca/hec/tenjin/api/TenjinSecurityService.java
+++ api/src/main/java/ca/hec/tenjin/api/TenjinSecurityService.java
@@ -1,16 +1,10 @@
 package ca.hec.tenjin.api;
 
-import ca.hec.tenjin.api.exception.NoSiteException;
 import ca.hec.tenjin.api.model.syllabus.AbstractSyllabus;
-import ca.hec.tenjin.api.model.syllabus.Syllabus;
 import org.sakaiproject.site.api.Group;
 import org.sakaiproject.site.api.Site;
 
-import java.util.List;
-
 public interface TenjinSecurityService {
-
-
 
 	/**
 	 * Checks whether the user has the permission in the specified section .
@@ -19,8 +13,7 @@ public interface TenjinSecurityService {
 	 * @param sectionRef
 	 * @return true or false
 	 */
-	public boolean check (String userId, String permission, Group sectionRef);
-
+	public boolean check(String userId, String permission, Group sectionRef);
 
 	/**
 	 * Checks whether the user has the permission for the specified syllabus.
@@ -32,91 +25,12 @@ public interface TenjinSecurityService {
 	 */
 	public boolean check(String userId, String permission,  AbstractSyllabus syllabus);
 
-
-	public boolean checkOnSiteGroup (String userId, String permission, Site site);
-
 	/**
-	 * Return the sections (with permissions) for the current user
-	 * @param permissions Get the permissions (optional)
-	 * @return
-	 */
-	public List<String> getSections(String userId, String siteId) throws NoSiteException;
-
-/*	*//**
-	 * Static variable used to represent the section (or users) associated to the site
-	 *//*
-	public static final String TENJIN_SITE_SECTION = "SITE";
-	*//**
-	 * This user has all the rights in an syllabus or template
-	 * @param userId
-	 * @return
-	 *//*
-	public boolean isSuperUser(String userId);
-	*//**
-	 * Checks whether the user has the permission for the common syllabus
+	 * Checks whether the user has the permission for the site realm.
 	 * @param userId
 	 * @param permission
-	 * @return
-	 *//*
-	public boolean isAllowedCommon(String userId, String permission);
-
-
-
-
-	/**
-	 * Return an array with the sections for which the current user has the given permission
-	 * from TenjinFunctions.
-	 *
-	 * @param String permission
-	 * @return List of section ids
-	 *//*
-	public List<String> getArraySections(String siteId, String permission);
-
-	*//**
-	 * Return whether or not the user is allowed to create a syllabus (common or not)
-	 * in the given site
-	 *
-	 * @param String siteId
-	 * @param boolean common - true to check permission for common syllabus
-	 * @return boolean
-	 *//*
-	public boolean canUserCreateSyllabus(String siteId, boolean common);
-
-	*//**
-	 * Return whether or not the user is allowed to update a given syllabus
-	 *
-	 * @param Syllabus syllabus
-	 * @return boolean
-	 *//*
-	public boolean canUserUpdateSyllabus(Syllabus syllabus);
-
-	*//**
-	 * Return whether or not the user is allowed to assign or unassign all of the specified
-	 * sections in the collection
-	 *
-	 * @param Collection<String> section ids to verify
-	 * @return boolean
-	 *//*
-	public boolean canUserAssignSections(String siteId, Collection<String> sectionsToCheck);
-
-	*//**
-	 * Check if the user has the right to see the management page: is a super user or has @see TenjinFunctions#TENJIN_FUNCTION_VIEW_MANAGER on site
-	 * @param siteId
+	 * @param site
 	 * @return true or false
-	 *//*
-	public boolean viewManagementPage(String siteId);
-
-	*//**
-	 * Check if the user is allowed to see the published common course outline: is super user or has @see TenjinFunctions#TENJIN_FUNCTION_READ on site or has @see      TenjinFunctions#TENJIN_FUNCTION_WRITE on site
-	 * @param siteId
-	 * @param userId
-	 * @return true or false
-	 *//*
-	public boolean read(String siteId, String userId);
-
-
-	public List<String> write (String siteId);
-
-	public List<String> publish(String siteId);*/
-
+	 */
+	public boolean checkOnSiteGroup(String userId, String permission, Site site);
 }
