72eb0572a80ecf4b2a5daf5b2fe43816e680d45d
Mame Awa Diop <mame-awa.diop@hec.ca>
2017-04-10 16:33:55 -0400

    ZCII-2919: Details de l'examen manquantes -Ajout des détails de l'évaluation

--- tool/src/main/webapp/source/components/element/evaluationattrElement/evaluationattrElement.js
+++ tool/src/main/webapp/source/components/element/evaluationattrElement/evaluationattrElement.js
@@ -2,20 +2,23 @@ tenjinApp.directive('evaluationattrElement', function() {
     'use strict';
 
     return {
-        scope: true,
+        scope: {
+            element: '=evaluationattrElement'
+        },
 
         restrict: 'A',
 
         templateUrl: 'element/evaluationattrElement/evaluationattrElement.html',
 
         controller: function($scope) {
-
+             console.log($scope.element.attributes);
         },
 
         link: function($scope, $element) {
             $scope.slash = '/';
             $scope.team = 'team';
             $scope.individual = 'individual';
+           
         }
     };
 });
--- tool/src/main/webapp/source/components/element/examattrElement/examattrElement.js
+++ tool/src/main/webapp/source/components/element/examattrElement/examattrElement.js
@@ -2,14 +2,16 @@ tenjinApp.directive('examattrElement', function() {
     'use strict';
 
     return {
-        scope: true,
+        scope: {
+            element: '=examattrElement'
+        },
 
         restrict: 'A',
 
         templateUrl: 'element/examattrElement/examattrElement.html',
 
         controller: function($scope) {
-
+            console.log($scope.element.attributes);
         },
 
         link: function($scope, $element) {
