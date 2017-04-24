bbf750a990bd376622af8872fdec51ac02c4f5cc
simonggauthierhec <simon.gregoire-gauthier@hec.ca>
2017-04-24 16:33:41 -0400

    ZCII-2813

--- tool/src/main/webapp/source/components/form/contactElementForm/contactElementForm.js
+++ tool/src/main/webapp/source/components/form/contactElementForm/contactElementForm.js
@@ -1,63 +1,69 @@
 ﻿tenjinApp.directive('contactElementForm', ['config', '$translate', function(config, $translate) {
-    'use strict';
+	'use strict';
 
-    return {
-        scope: true,
+	return {
+		scope: true,
 
-        restrict: 'A',
+		restrict: 'A',
 
-        templateUrl: 'form/contactElementForm/contactElementForm.html',
+		templateUrl: 'form/contactElementForm/contactElementForm.html',
 
-        controller: function($scope) {
-            $scope.config = config;
-            // setup editor options
-            $scope.editorOptionsDisponibilite = {
-                language: 'fr',
-                height: '120',
-                toolbar: config.ckeditorToolbarTenjin,
-                removePlugins: 'elementspath,resize'
-            };
+		controller: function($scope) {
+			$scope.config = config;
+			// setup editor options
+			$scope.editorOptionsDisponibilite = {
+				language: 'fr',
+				height: '120',
+				toolbar: config.ckeditorToolbarTenjin,
+				removePlugins: 'elementspath,resize'
+			};
 
-            $scope.editorOptionsCommentaire = {
-                language: 'fr',
-                height: '120',
-                toolbar: config.ckeditorToolbarTenjin,
-                removePlugins: 'elementspath,resize'
-            };
+			$scope.editorOptionsCommentaire = {
+				language: 'fr',
+				height: '120',
+				toolbar: config.ckeditorToolbarTenjin,
+				removePlugins: 'elementspath,resize'
+			};
 
-            $scope.selectTitle = function($title) {
-                $scope.currentTitle = $title.name;
-                $scope.element.attributes.contactInfoTitle = $scope.currentTitle;
-            };
+			$scope.getContactInfoTitles = function () {
+				return config.contactInfoTitles.sort(function(a, b) {
+					return $translate.instant(a.name).localeCompare($translate.instant(b.name));
+				});
+			};
 
-            $scope.element.validate = function() {
-                var ret = [];
+			$scope.selectTitle = function($title) {
+				$scope.currentTitle = $title.name;
+				$scope.element.attributes.contactInfoTitle = $scope.currentTitle;
+			};
 
-                if (!this.attributes.contactInfoFirstName || this.attributes.contactInfoFirstName.length <= 0) {
-                    ret.push({
-                        field: "contactInfoFirstName",
-                        message: "ERROR_MISSING_CONTACTINFO_FIRSTNAME"
-                    });
-                }
-                if (!this.attributes.contactInfoLastName || this.attributes.contactInfoLastName.length <= 0) {
-                    ret.push({
-                        field: "contactInfoLastName",
-                        message: "ERROR_MISSING_CONTACTINFO_LASTNAME"
-                    });
-                }
+			$scope.element.validate = function() {
+				var ret = [];
 
-                return ret;
-            }
-        },
-        
-        link: function($scope, $element) {
-            // Retrieve the title for the given title id
-            if ($scope.element.attributes.contactInfoTitle) {
+				if (!this.attributes.contactInfoFirstName || this.attributes.contactInfoFirstName.length <= 0) {
+					ret.push({
+						field: "contactInfoFirstName",
+						message: "ERROR_MISSING_CONTACTINFO_FIRSTNAME"
+					});
+				}
+				if (!this.attributes.contactInfoLastName || this.attributes.contactInfoLastName.length <= 0) {
+					ret.push({
+						field: "contactInfoLastName",
+						message: "ERROR_MISSING_CONTACTINFO_LASTNAME"
+					});
+				}
+
+				return ret;
+			}
+		},
+		
+		link: function($scope, $element) {
+			// Retrieve the title for the given title id
+			if ($scope.element.attributes.contactInfoTitle) {
 				$scope.currentTitle = $scope.element.attributes.contactInfoTitle;
-            } else {
-                $scope.currentTitle = $scope.config.contactInfoTitles[0].name;
-            }
-        }
-    };
+			} else {
+				$scope.currentTitle = $scope.config.contactInfoTitles[0].name;
+			}
+		}
+	};
 
 }]);
