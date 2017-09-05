634dd9232724e130dd83ee48b6b4a63d15fe51b8
Curtis van Osch <curtis.van-osch@hec.ca>
2017-09-05 14:37:59 -0400

    ZCII-3089: verify write permission on common when displaying the import button

--- tool/src/main/java/ca/hec/tenjin/tool/controller/UserController.java
+++ tool/src/main/java/ca/hec/tenjin/tool/controller/UserController.java
@@ -107,7 +107,8 @@ public class UserController {
 			}
 
 			// Whether to allow import
-			profile.put("activateImportButton", importProvider != null);
+			profile.put("activateImportButton", importProvider != null &&
+					securityService.checkOnSiteGroup(currentUserId, TenjinFunctions.TENJIN_FUNCTION_WRITE_COMMON, site));
 
 			// The user has permissions in the sections
 			boolean writeOnSite = securityService.checkOnSiteGroup(currentUserId, TenjinFunctions.TENJIN_FUNCTION_WRITE_PERS, site);
