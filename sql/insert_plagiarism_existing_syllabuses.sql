64553fa37adabc5006057fe9b32e50f9de533850
Curtis van Osch <curtis.van-osch@hec.ca>
2017-09-12 10:52:32 -0400

    Change query to only find non-deleted syllabuses, and retrieve syllabus locale, and add debug output

--- sql/insert_plagiarism_existing_syllabuses.sql
+++ sql/insert_plagiarism_existing_syllabuses.sql
@@ -1,26 +1,25 @@
 set serveroutput on size unlimited;
 set define off;
--- for each common evaluation element
--- insert into tenjin syllabus element the rubric/text
--- create mapping for each associated personalised
--- reorder all children
 DECLARE
   CURSOR common_evaluation_elements_cur is
-    SELECT syllabuselement_id, site_id FROM tenjin_syllabuselement 
-    WHERE common = 1
-    AND templatestructure_id = 12
-    AND class_type = 'ca.hec.tenjin.api.model.syllabus.SyllabusCompositeElement'
---    AND site_id LIKE '%._201_.%'
-    AND site_id LIKE '30-400-17.A2017'
-    ORDER BY site_id;
+    SELECT e.syllabuselement_id, s.site_id, locale FROM tenjin_syllabuselement e, tenjin_syllabuselementmapping m, tenjin_syllabus s
+    WHERE e.syllabuselement_id = m.syllabuselement_id
+    AND m.syllabus_id = s.syllabus_id
+    AND s.deleted = 0
+    AND s.common = 1
+    AND e.common = 1
+    AND e.templatestructure_id = 12
+    AND s.site_id LIKE '30-400-17.A2017'
+    AND e.class_type = 'ca.hec.tenjin.api.model.syllabus.SyllabusCompositeElement';
     
   evaluations_element_id NUMBER(19,0);
   rubric_id NUMBER;
   text_element_id NUMBER;
   site_id VARCHAR2(255 CHAR);
-  locale VARCHAR2(5 CHAR) := 'fr_CA';
+  locale VARCHAR2(5 CHAR);
   rubric_title VARCHAR2(255 CHAR);
   text_description CLOB;
+  n NUMBER := 0;
   
 BEGIN
   dbms_output.enable(NULL);
@@ -28,14 +27,14 @@ BEGIN
   OPEN common_evaluation_elements_cur;
 
   LOOP
-    FETCH common_evaluation_elements_cur INTO evaluations_element_id, site_id;
+    FETCH common_evaluation_elements_cur INTO evaluations_element_id, site_id, locale;
     EXIT WHEN common_evaluation_elements_cur%NOTFOUND;
      
     IF locale = 'en_US' THEN
       rubric_title := 'IMPORTANT';
       text_description := '<h3>Plagiarism</h3><p>Students are expected to become informed of which actions are considered to be plagiarism or other academic violations at HEC Montr&eacute;al, and of the applicable procedure and sanctions, which may include suspension or even expulsion from HEC Montr&eacute;al. Violations are judged based on the facts and circumstances, and sanctions are applied accordingly. <a href="http://www.hec.ca/en/programs_training/plagiarism.html" targe="_black">Learn more about plagiarism...</a></br>Cases of plagiarism are often caused by documented sources and in-text citations not being referenced correctly.</br>All forms of assignments can be analyzed using originality checking software.</br>You are invited to consult the guide "<a href="http://libguides.hec.ca/citing" target="_blank">Citing your sources</a>", from the reference helpdesk of the HEC library.</p><h3>Calculators</h3><p>Please consult the <a href="http://www.hec.ca/en/current_student/technologies/authorized_calculators/calculators.html" target="_blank">calculator usage policy</a> during exams when applicable.<h3>Mutual aid during an individual work</h3><p>It is the opinion of HEC Montreal that students engaging in discussion and collaboration for their individual work or paper, is not only normal, but also desirable. These interactions contribute to the enrichment of the overall learning process.</br>However, there are certain types of communication between students that infringe upon the School’s rules and regulations regarding plagiarism. Transmitting files, ongoing discussions resulting in an agreement on a set process and the answer per se are considered as such infringements.</p>';
-    ELSE IF locale = 'es_ES' THEN
-      rubric_title := 'spanish title';
+    ELSIF locale = 'es_ES' THEN
+      rubric_title := 'IMPORTANTE';
       text_description := 'spanish text';
     ELSE
       rubric_title := 'IMPORTANT';
@@ -64,6 +63,9 @@ BEGIN
       (syllabuselmap_id_seq.NEXTVAL, mappings.syllabus_id, text_element_id, 0, 0);
     END LOOP;
 
+  n := n+1;
+  dbms_output.put_line('Inserted rubric ' || rubric_id || ' into common syllabus for site ' || site_id);
   END LOOP;
+  dbms_output.put_line('New rubric inserted in ' || n || ' syllabuses');
   CLOSE common_evaluation_elements_cur;
 END;
