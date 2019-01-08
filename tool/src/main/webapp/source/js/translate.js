79f1c9d947c01708a69917b792da44c171bb49f0
simon gregoire-gauthier <simon.gregoire-gauthier@hec.ca>
2019-01-08 11:34:38 -0500

    Enable spanish ui

--- tool/src/main/webapp/source/js/translate.js
+++ tool/src/main/webapp/source/js/translate.js
@@ -4,7 +4,7 @@
 	$translateProvider.useSanitizeValueStrategy('escape');
 
 	$translateProvider.preferredLanguage('fr_CA');
-	$translateProvider.registerAvailableLanguageKeys(['en_US', 'fr_CA']);
+	$translateProvider.registerAvailableLanguageKeys(['en_US', 'fr_CA', 'es_ES']);
 
 	$translateProvider.useUrlLoader('v1/strings.json');
 }]);
