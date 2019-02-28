a018e5fb13999750e43c88e197a65c4cd4841a22
Curtis van Osch <curtis.van-osch@hec.ca>
2019-02-28 12:04:54 -0500

    ZCII-3544 : new exception for actions that cannot be taken when syllabus is published

---
+++ api/src/main/java/ca/hec/tenjin/api/exception/PublishedSyllabusException.java
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * $Id: $
+ ******************************************************************************
+ *
+ * Copyright (c) 2015 The Sakai Foundation, The Sakai Quebec Team.
+ *
+ * Licensed under the Educational Community License, Version 1.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+package ca.hec.tenjin.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author <a href="mailto:mame-awa.diop@hec.ca">Mame Awa Diop</a>
+ * @version $Id: $
+ */
+public class PublishedSyllabusException extends SyllabusException{
+	
+	@Getter
+	Long syllabusId;
+
+    public PublishedSyllabusException( ){
+	super(HttpStatus.PRECONDITION_FAILED);
+	this.setHttpStatus(HttpStatus.PRECONDITION_FAILED);
+    }
+
+    public PublishedSyllabusException(Long syllabusId){
+    	super(HttpStatus.PRECONDITION_FAILED);
+    	this.setHttpStatus(HttpStatus.PRECONDITION_FAILED);
+    	
+    	this.syllabusId = syllabusId;
+    }
+}
+
