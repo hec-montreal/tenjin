d2bdda5583e48838c0b09237195e8a6683327f86
Mame Awa Diop <mame-awa.diop@hec.ca>
2021-10-28 11:19:03 -0400

    ZCII-4594: Lister les sites du 2+2 qui utilisent un template officiel et voir la possibilité de migrer vers un template non officiel

--- sql/convert-template.sql
+++ sql/convert-template.sql
@@ -1,4 +1,4 @@
---données format csv
+--donnï¿½es format csv
 set serveroutput on size unlimited;
 set define off;
 DECLARE
@@ -27,25 +27,29 @@ BEGIN
    select * bulk collect into syllabusElements from tenjin_syllabuselement where syllabuselement_id in 
    (select syllabuselement_id from TENJIN_SYLLABUSELEMENTMAPPING tsm, TENJIN_SYLLABUS ts where tsm.SYLLABUS_ID = ts.SYLLABUS_ID 
    and ts.SITE_ID = siteID and ts.DELETED = 0) order by TEMPLATESTRUCTURE_ID;
+
+   if syllabusElements.count > 0 then
+     for idx in syllabusElements.first().. syllabusElements.last() loop
+         --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || syllabusElements(idx).TEMPLATESTRUCTURE_ID); 
+        if (syllabusElements(idx).TEMPLATESTRUCTURE_ID > 0) then
    
-   for idx in syllabusElements.first().. syllabusElements.last() loop
-       --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || syllabusElements(idx).TEMPLATESTRUCTURE_ID); 
-      if (syllabusElements(idx).TEMPLATESTRUCTURE_ID > 0) then
- 
-        select mapId into mappingId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
-        TEMPLATE_ID,  TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
-        START WITH TEMPLATE_ID =1
-        CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where templatestructure_id =syllabusElements(idx).TEMPLATESTRUCTURE_ID; 
-          
-        select templatestructure_id into newTemplateStructureId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
-        TEMPLATE_ID, TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
-        START WITH TEMPLATE_ID =2
-        CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where mapId = mappingId;
-  
-        update TENJIN_SYLLABUSELEMENT set TEMPLATESTRUCTURE_ID = newTemplateStructureId where syllabuselement_id = syllabusElements(idx).syllabuselement_id;
-        --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || syllabusElements(idx).TEMPLATESTRUCTURE_ID || ' to templateStructure ' || newTemplateStructureId);                          
-        end if;
-   end loop;
+          select mapId into mappingId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
+          TEMPLATE_ID,  TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
+          START WITH TEMPLATE_ID =1
+          CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where templatestructure_id =syllabusElements(idx).TEMPLATESTRUCTURE_ID; 
+            
+          select templatestructure_id into newTemplateStructureId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
+          TEMPLATE_ID, TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
+          START WITH TEMPLATE_ID =2
+          CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where mapId = mappingId;
+    
+          update TENJIN_SYLLABUSELEMENT set TEMPLATESTRUCTURE_ID = newTemplateStructureId where syllabuselement_id = syllabusElements(idx).syllabuselement_id;
+          --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || syllabusElements(idx).TEMPLATESTRUCTURE_ID || ' to templateStructure ' || newTemplateStructureId);                          
+          end if;
+     end loop;
+   else
+    DBMS_OUTPUT.PUT_LINE( 'Pas de plan de cours');
+   end if;  
    
    -- update published syllabus
    
@@ -53,24 +57,27 @@ BEGIN
    select * bulk collect into pubSyllabusElements from TENJIN_PUBSYLLABUSELEMENT where PUBSYLLABUSELEMENT_ID in 
    (select tsm.PUBSYLLABUSELEMENT_ID from TENJIN_PUBSYLLABUSELEM_MAPPING tsm, TENJIN_SYLLABUS ts where tsm.SYLLABUS_ID = ts.SYLLABUS_ID 
    and ts.SITE_ID = siteID and ts.DELETED = 0) order by TEMPLATESTRUCTURE_ID;
-   
-   for idx in pubSyllabusElements.first().. pubSyllabusElements.last() loop
-     --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID); 
-     if (pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID > 0) then
- 
-       select mapId into mappingId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
-       TEMPLATE_ID,  TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
-       START WITH TEMPLATE_ID =1
-       CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where templatestructure_id =pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID; 
-       
-       select templatestructure_id into newTemplateStructureId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
-       TEMPLATE_ID, TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
-       START WITH TEMPLATE_ID =2
-       CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where mapId = mappingId;
-  
-       update TENJIN_PUBSYLLABUSELEMENT set TEMPLATESTRUCTURE_ID = newTemplateStructureId where PUBSYLLABUSELEMENT_ID = pubSyllabusElements(idx).pubsyllabuselement_id;
-       --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID || ' to templateStructure ' || newTemplateStructureId);                          
-     end if;
-   end loop;
-END;
 
+   if pubSyllabusElements.count > 0 then
+     for idx in pubSyllabusElements.first().. pubSyllabusElements.last() loop
+       --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID); 
+       if (pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID > 0) then
+   
+         select mapId into mappingId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
+         TEMPLATE_ID,  TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
+         START WITH TEMPLATE_ID =1
+         CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where templatestructure_id =pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID; 
+         
+         select templatestructure_id into newTemplateStructureId from (SELECT level, TEMPLATESTRUCTURE_ID, PARENT_ID,  DISPLAY_ORDER, SYS_CONNECT_BY_PATH(TEMPLATEELEMENT_ID, '/') mapId,
+         TEMPLATE_ID, TEMPLATEELEMENT_ID,  SYS_CONNECT_BY_PATH(TEMPLATESTRUCTURE_ID, '/') "Path2" FROM TENJIN_TEMPLATESTRUCTURE
+         START WITH TEMPLATE_ID =2
+         CONNECT BY  prior TEMPLATESTRUCTURE_ID =  PARENT_ID order by level, DISPLAY_ORDER, mapId) where mapId = mappingId;
+    
+         update TENJIN_PUBSYLLABUSELEMENT set TEMPLATESTRUCTURE_ID = newTemplateStructureId where PUBSYLLABUSELEMENT_ID = pubSyllabusElements(idx).pubsyllabuselement_id;
+         --DBMS_OUTPUT.PUT_LINE( 'The syllabusElement has been updated ' || pubSyllabusElements(idx).TEMPLATESTRUCTURE_ID || ' to templateStructure ' || newTemplateStructureId);                          
+       end if;
+     end loop;
+   else
+    DBMS_OUTPUT.PUT_LINE( 'Pas de plan de cours publie');
+   end if;  
+END;
